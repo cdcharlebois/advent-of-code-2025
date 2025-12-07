bd9bf3eed8d0f980ea25f3294ca506ace25ed414
cdcharlebois <cdcharlebois@gmail.com>
2025-12-07 11:15:13 -0500

    add day 7

--- index.ts
+++ index.ts
@@ -4,13 +4,14 @@ import { part1, part2 } from "./src/day3";
 import day4 from "./src/day4";
 import day5 from "./src/day5";
 import day6 from "./src/day6";
+import day7 from "./src/day7";
 
 const fs = require("node:fs/promises");
 
 
 (async () => {
-  const input = await fs.readFile("./data/day6.txt", "utf-8");
-  const output = day6.part2(input);
+  const input = await fs.readFile("./data/day7.txt", "utf-8");
+  const output = day7.part2(input);
   console.log({ output });
 })();
 
