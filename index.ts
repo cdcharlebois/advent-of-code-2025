646a3faed1ae78e3201f2feeb3a0c55a01a95fe6
Conner Charlebois <connercharlebois@stripe.com>
2025-12-10 09:23:18 -0500

    scaffold day 10

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
+import day10 from "./src/day10.js";
 import day8_3 from "./src/day8_3.js";
 import fs from "node:fs/promises"
 
 
 (async () => {
   const input = await fs.readFile("./data/day8.txt", "utf-8");
-  const output = day8_3.part1(1000, input);
+  const output = day10.part1();
   // const output = day8_3.part1(10);
   console.log({ output });
 })();
-
